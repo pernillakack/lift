d90ebfc3aa0509fe6dffc42ac2e3b4eb5e72ed16
pernillakack <112933591+pernillakack@users.noreply.github.com>
2023-04-04 15:33:40 +0200

    test med console.log/med Simon

--- Components/shoulders.tsx
+++ Components/shoulders.tsx
@@ -1,29 +1,30 @@
+import { stringify } from "querystring";
+import { MouseEvent, useState } from "react";
+
+
 function Shoulders() {
    const shoulders = ["Overhead dumbbell press","Overhead barbell press","Face pull","Dumbbell rear delt row","Lateral dumbbell raises","Shrugs"]
-return (
+   
+   
+
+   return (
     <>
     <form>
     <ul className="exercises">
-       
-    </ul>
       <select name ="axlar">
         <option className="">Axlar</option>
       {shoulders.map(shoulders =>
-        <option className="" key={shoulders} onClick={(event)=> console.log(shoulders)}>{shoulders}</option>)}  
-      {/*  <option value="Axlar">Axlar</option>
-        <option value="Overhead dumbbell press">Overhead dumbbell press</option>
-        <option value="Overhead barbell press">Overhead barbell press</option>
-        <option value="Face pull">Face pull</option>
-        <option value="Dumbbell rear delt row">Dumbbell rear delt row</option>
-        <option value="Lateral dumbbell raises ">Lateral dumbbell raises</option>
-        <option value="Shrugs">Shrugs</option>*/}
+        <option className=" list-item active" key={shoulders} onClick={() =>console.log(shoulders)}>{shoulders}</option>)}  
       </select>
       <br />
       <select name ="ben">
         <option value="Ben">Ben</option>
       
       </select>
+      </ul>
     </form>
+    
+    
     </>
 )
 }
