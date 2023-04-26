8971cced735b521d5831c81863f285dc3fd02a2b
pernillakack <112933591+pernillakack@users.noreply.github.com>
2023-04-26 18:12:11 +0200

    adjusted margins in and between the cards

--- Components/excerciseCard.tsx
+++ Components/excerciseCard.tsx
@@ -28,9 +28,10 @@ exerciseContext.setMuscleGroup(element.muscleGroup);
 
   const listExercises = exercises.map(
     (element) => (
+      <div className=' m-4'>
       <li key={`${element.id}`}>
-        <div onClick={() => handleExercise(element)} id='card' className='flex justify-between px-4 py-4 h-20 bg-white rounded-lg shadow-[4px 5px 15px rgba(0,0,0,0.07)] w-[342px] left-[20px] top-[20px] m-8'>
-          <div id='info' className='flex flex-col'>{`${element.exercise}`} br
+        <div onClick={() => handleExercise(element)} id='card' className='flex justify-between px-4 py-4 h-20 bg-white rounded-lg shadow-[4px 5px 15px rgba(0,0,0,0.07)] w-[342px] left-[20px] top-[20px]'>
+          <div id='info' className='block'>{`${element.exercise}`}
             <div id='muskelgrupp' className=' py-2 w-24 h-4 not-italic font-normal text-xs leading-4'>{`${element.muscleGroup}`}</div>
           </div>
           <div id='container plus' className='flex my-4 ml-1 mr-2'>
@@ -38,6 +39,7 @@ exerciseContext.setMuscleGroup(element.muscleGroup);
           </div>
         </div>
       </li>
+      </div>
     )
   )
 
