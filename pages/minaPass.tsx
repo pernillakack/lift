68e7995abbe03009976ebf4124ac94feaed8e678
Simon Nilsson <simonnilsson90@hotmail.com>
2023-04-28 22:34:19 +0200

    connectade Databs, renderade ut data mongoclient

--- pages/minaPass.tsx
+++ pages/minaPass.tsx
@@ -1,10 +1,8 @@
+/*
 import { NextPage, GetServerSideProps } from 'next'
 import { Workout } from '@/types/workout'
 import { connectToDatabase } from '@/utils/db'
 import { WorkoutModel } from '@/schemas/workoutSchema'
-
-
-
 
 type Props = {
     workout: Workout []
@@ -56,3 +54,89 @@ export const getServerSideProps: GetServerSideProps<any> = async () => {
     },
   }
 }
+
+*/
+
+import { NextPage } from 'next'
+import { GetStaticProps } from "next";
+import { MongoClient } from "mongodb";
+
+import { InferGetStaticPropsType } from "next";
+import { ObjectId } from "mongodb";
+
+export type Workout = {
+    musclegroup: string;
+    _id: number;
+    weight: number;
+    name: string; 
+    exercise: string;
+    reps: number;
+    sets: number
+    };
+
+   
+    
+export const getStaticProps: GetStaticProps = async (context) => {
+        const mongoClient = new MongoClient(
+            'mongodb+srv://simonnilsson90:Lattelotta@cluster0.ltg2z5p.mongodb.net/liftdatabase'
+            
+            );
+
+            const data = await mongoClient
+            .db()
+            .collection('myworkout')
+            .find()
+            .toArray();
+
+            console.log( '!!!',data);
+
+            return {
+                props: {
+                    workouts: JSON.parse(JSON.stringify(data)),
+                },
+                revalidate: 60
+        
+            };
+        };
+
+        const Workout: NextPage = ({
+            workouts,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+return (
+<>
+<div className=' h-screen flex justify-center'>
+    <div className=" bg-cover w-full flex items-center justify-center"
+  style={{
+    backgroundImage:
+      "url('https://4599529af2.cbaul-cdnwnd.com/801f178300d1552abc2bf838876c8a02/200000001-58c0f58c11/liftstart.webp?ph=4599529af2')",
+   maxWidth:700}}> 
+  
+     <br /> <br />
+      {workouts.map((workout: Workout) => {
+     return (
+        
+        <div key={workout._id}>
+            <h1>Mina Pass</h1>
+            <br />
+            <p>{workout.name}</p>
+            <p>{workout.musclegroup}</p>
+            <p>Sets:{workout.sets}</p>
+            <p>Reps:{workout.reps}</p>
+            
+
+
+        </div>
+        
+        );
+   
+})}
+</div>
+ </div>
+</>
+
+);
+};
+
+export default Workout
+
+ 
