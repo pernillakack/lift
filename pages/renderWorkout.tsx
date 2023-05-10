f7ae7d310931e6e674c947679da98f6b8b118c20
Simon Nilsson <simonnilsson90@hotmail.com>
2023-05-10 03:30:04 +0200

    La till button och bg

--- pages/renderWorkout.tsx
+++ pages/renderWorkout.tsx
@@ -1,17 +1,30 @@
 import { NextPage } from 'next'
 import { Workout } from '@/types/workout'
+import Button from '@/Components/button'
+import Link from 'next/link'
+import { MouseEvent } from 'react'
 
+
+
+
+const handleClick = async () => {
+  
+  console.log("Gå till loggade pass")
+
+}
 
 type Props = {
   workouts: Workout []
 }
 
+
+
 const RenderWorkout: NextPage<Props> = ({workouts}) => {
   return( 
-  <div className="">
+<div className=' uxBackground h-screen block '> 
     <form className=' py-12 flex justify-center'>
     <div >
-    <h1>Face Pull</h1>
+    <h1>Tisdag</h1>
     </div>
     </form>
     
@@ -23,10 +36,21 @@ const RenderWorkout: NextPage<Props> = ({workouts}) => {
             <div className=' mr-6'>{} Kg</div>
         </li>
     </ul>
-
+    <Link href="/minaPass">
+  <div className="fixed bottom-8 left-1/2 transform -translate-x-1/2">
+    <Button text="Logga pass >" onClick={handleClick} />
   </div>
-  )
-}
+</Link>
+    
+  </div>
+
+   
+
+  
+ 
+  
+  
+  )}
 
 
 export default RenderWorkout
